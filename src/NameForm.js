c7a15d431d73748fc261d80e87fe439fbf9014fe
Michael Howell <michaelhowellrt@hotmail.com>
2018-08-23 15:13:08 -0400

    name submission locked

--- src/NameForm.js
+++ src/NameForm.js
@@ -3,12 +3,29 @@ import React, { Component } from 'react';
 class NameForm extends Component {
     constructor(props) {
       super(props);
-      this.state = {value: 'Please enter your name'}; 
+      this.state = {value: 'Please enter your name',
+                    nameNotPlaced: true}; 
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
       this.placeName = this.placeName.bind(this);
+      this.namePlacement = this.namePlacement.bind(this);
     }
     
+    namePlacement = () => {
+      if (this.state.nameNotPlaced === true){
+        return ( 
+          <form onSubmit={this.handleSubmit}>
+          <label>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        )
+      }else {
+        return (<p> </p>)
+      }
+    }
+
     placeName = () => {
       this.props.onNameInput(this.state.value);
     }
@@ -19,18 +36,13 @@ class NameForm extends Component {
   
     handleSubmit(event) {
       event.preventDefault();
-      this.placeName()
-      
+      this.placeName();
+      this.setState({nameNotPlaced: false})
     }
   
     render() {
       return (
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+       <div>{this.namePlacement()}</div>
       );
     }
   }
